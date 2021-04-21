0b593bd0543b16385f7155b9997dcdc6577b35e5
vpscommacc <vps.commacc@yandex.ru>
2021-04-21 18:15:11 +0300

    progressing

--- main.py
+++ main.py
@@ -46,10 +46,40 @@ canvas.create_rectangle(
 def xs():
     canvas.create_rectangle(
         100, 240, 160, 300,
+        outline="black", fill="red", width=4
+    )
+
+def ls():
+    canvas.create_rectangle(
+        200, 210, 160, 300,
         outline="black", fill="yellow", width=4
+    )
+    canvas.create_rectangle(
+        140, 210, 160, 240,
+        outline="black", fill="yellow", width=4
+    )
+
+def ks1():
+    canvas.create_rectangle(
+        100, 170, 140, 240,
+        outline="black", fill="blue", width=4
+    )
+    canvas.create_rectangle(
+        100, 170, 140, 240,
+        outline="black", fill="blue", width=4
+    )
+
+def clear():
+    canvas.create_rectangle(
+        100, 100, 200, 300,
+        outline="black", fill="white", width=4
     )
 
 
 create_grid(window)
 Button(window, text="Квадрат", command=xs).place(relx=.0.real, rely=.943)
+Button(window, text="L наоборот", command=ls).place(relx=.172.real, rely=.943)
+Button(window, text="1-ый уголок", command=ks1).place(relx=.378.real, rely=.943)
+Button(window, text="2-ой уголок", command=ls).place(relx=.606.real, rely=.943)
+Button(window, text="Очистка", command=clear).place(relx=.828.real, rely=.943)
 window.mainloop()
